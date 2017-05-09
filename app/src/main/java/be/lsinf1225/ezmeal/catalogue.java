82d3bc7a49fa7e6922dc4269fcbbb6d602e30939
GGortz <gaetan.gortz@student.uclouvain.be>
2017-05-09 19:07:43 +0200

    catalogueok

--- app/src/main/java/be/lsinf1225/ezmeal/catalogue.java
+++ app/src/main/java/be/lsinf1225/ezmeal/catalogue.java
@@ -1,37 +1,41 @@
 package be.lsinf1225.ezmeal;
 
-        import android.app.ProgressDialog;
-        import android.content.Context;
-        import android.content.Intent;
-        import android.graphics.Bitmap;
-        import android.os.AsyncTask;
-        import android.os.Bundle;
-        import android.support.v7.app.ActionBarActivity;
-        import android.view.LayoutInflater;
-        import android.view.Menu;
-        import android.view.MenuItem;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.AdapterView;
-        import android.widget.ArrayAdapter;
-        import android.widget.ImageView;
-        import android.widget.ListView;
-        import android.widget.ProgressBar;
-        import android.widget.RatingBar;
-        import android.widget.TextView;
-        import android.widget.Toast;
-        import java.io.BufferedReader;
-        import java.io.IOException;
-        import java.io.InputStream;
-        import java.io.InputStreamReader;
-        import java.net.HttpURLConnection;
-        import java.net.MalformedURLException;
-        import java.net.URL;
-        import java.util.ArrayList;
-        import java.util.List;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Catalogue extends AppCompatActivity {
+
+        ListView catalogue;
 
 
-public class catalogue{
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_catalogue);
+
+                catalogue = (ListView) findViewById(R.id.cataloguederecette);
+                ArrayAdapter<String> catalogueAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.catalogue));
+                catalogue.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+                                Intent intent = new Intent(Catalogue.this, AfficherRecette.class);
+                                intent.putExtra("categories", catalogue.getItemAtPosition(i).toString());
+                                startActivity(intent);
+                        }
+                });
 
 
+
+
+
+                catalogue.setAdapter(catalogueAdapter);
+
+
+
+        }
 }
